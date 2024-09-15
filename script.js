5ad180d2808913e79948a94200d61aa958b4d28b
Carlos Q <carlos.quezada.dev@gmail.com>
2024-09-14 21:40:19 -0500

    Portfolio 1.0

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-function sendMail(){
-    let parms = {
-        name: document.getElementById("name").value,
-        email: document.getElementById("email").value,
-        subject: document.getElementById("subject").value,
-        message: document.getElementById("message").value
-    }
-    emailjs.send("service_81bzcak","template_xkwlu58", parms).then(alert("Email enviado con exito"))
-}
+// function sendMail(){
+//     let parms = {
+//         name: document.getElementById("name").value,
+//         email: document.getElementById("email").value,
+//         subject: document.getElementById("subject").value,
+//         message: document.getElementById("message").value
+//     }
+//     emailjs.send("service_81bzcak","template_xkwlu58", parms).then(alert("Email enviado con exito"))
+// }
